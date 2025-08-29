66a8c77864569e03b80f3f28e16063619d77c434
reach bheru <reachbheru@gmail.com>
2025-08-29 17:41:07 +0530

    created new dependencies

--- src/dependency/service_deps.js
+++ src/dependency/service_deps.js
@@ -1,9 +1,14 @@
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
 import { Validator } from "../modules/utils/validator.js";
 import { FileUtils } from "../modules/utils/fileUtils.js";
 import { Logger } from "../modules/utils/logger.js";
 import { CustomError } from "../modules/utils/customError.js";
 import { FileProcessorQueue } from "../modules/utils/processorQueue.js";
+import { chunkUtils } from "../modules/utils/chunkUtils.js";
+import { embeddingUtils } from "../modules/utils/embeddingUtils.js";
 
+dotenv.config();
 
 function getFileUtils() {
   return new FileUtils(getLogger(), getCustomError(), getValidator());
@@ -25,4 +30,25 @@ function getQueue() {
   return new FileProcessorQueue();
 }
 
-export { getCustomError, getFileUtils, getLogger, getQueue, getValidator };
+function getGoogleAi() {
+  return new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+}
+
+function getChunkUtils() {
+  return new chunkUtils();
+}
+
+function getEmbeddingUtils() {
+  return new embeddingUtils(getGoogleAi(), getFileUtils());
+}
+
+export {
+  getCustomError,
+  getFileUtils,
+  getLogger,
+  getQueue,
+  getValidator,
+  getGoogleAi,
+  getChunkUtils,
+  getEmbeddingUtils,
+};
