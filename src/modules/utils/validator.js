94cc2e6a3493c9a03a025a1c993d7d73423d4494
reach bheru <reachbheru@gmail.com>
2025-08-29 10:35:32 +0530

    modified validator

--- src/modules/utils/validator.js
+++ src/modules/utils/validator.js
@@ -1,24 +1,42 @@
 export class Validator {
   jsonValidator(jsObject) {
-    try {
-      if (
-        typeof jsObject === "object" &&
-        jsObject !== null &&
-        !Array.isArray(jsObject) &&
-        Object.keys(jsObject).length !== 0
-      ) {
-        for(const value of Object.values(jsObject)){
-          if(typeof value === "object"){
-            if(!this.jsonValidator(value)){
-              return false;
-            }
+  try {
+    if (typeof jsObject === "object" && jsObject !== null) {
+      for (const value of Object.values(jsObject)) {
+        if (typeof value === "object" && value !== null) {
+          if (!this.jsonValidator(value)) {
+            return false;
           }
-        };
-        return true;
+        }
       }
-      return false;
-    } catch (error) {
-      return false;
+      return true;
     }
+    return false;
+  } catch (error) {
+    return false;
   }
+}
+
+  // jsonValidator(jsObject) {
+  //   try {
+  //     if (
+  //       typeof jsObject === "object" &&
+  //       jsObject !== null &&
+  //       !Array.isArray(jsObject) &&
+  //       Object.keys(jsObject).length !== 0
+  //     ) {
+  //       for(const value of Object.values(jsObject)){
+  //         if(typeof value === "object"){
+  //           if(!this.jsonValidator(value)){
+  //             return false;
+  //           }
+  //         }
+  //       };
+  //       return true;
+  //     }
+  //     return false;
+  //   } catch (error) {
+  //     return false;
+  //   }
+  // }
 }
